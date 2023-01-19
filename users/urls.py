227f3f55201d9a792082b41cea93d5419c5ff3f2
@akashg12345 <akash.gautam29@gmail.com>
2023-01-19 10:57:20 +0530

    trail one

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path as url
+
+from users.views.login_view import sign_in
+from users.views.logout_view import sign_out
+from users.views.registration_view import registration
+
+urlpatterns = [
+    url(r'^registration/$', registration, name="registration"),
+    url(r'^$', sign_in, name="login"),
+    url(r'^logout/$', sign_out, name="logout"),
+]
